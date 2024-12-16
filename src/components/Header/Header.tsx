17616b541c3654453f0705a00a1263f1c5a46945
wallaceBertolaccini <wallace.bertolaccini@doublecheck.com.br>
2024-12-16 15:47:08 -0300

    feat: created header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+export default function Header() 
+{
+    return (
+        <header className="header">
+            <div className="header-container">
+                <div className="logo-container">
+                    <a href="https://google.com">
+                        <img src="https://maciell.adv.br/wp-content/uploads/2024/01/android-chrome-512x512-1.png" alt=""/>
+                    </a>
+                    <p>J RODRIGUES ADVOCACIA</p>
+                </div>
+            </div>
+        </header>
+    );
+}
