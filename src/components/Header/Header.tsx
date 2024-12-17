51e76decb8c255b5620d26ec1b11442320e97dc5
Marcelo Siqueira <marcelosiqqueira@gmail.com>
2024-12-17 11:36:23 -0300

    feat: created header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,45 @@
+import "./Header.css";
+
 export default function Header() 
 {
     return (
-        <header className="header">
+        <header>
             <div className="header-container">
                 <div className="logo-container">
                     <a href="https://google.com">
-                        <img src="https://maciell.adv.br/wp-content/uploads/2024/01/android-chrome-512x512-1.png" alt=""/>
+                        <img src="src\assets\images\logo.png" alt=""/>
                     </a>
-                    <p>J RODRIGUES ADVOCACIA</p>
+                    <p>J Rodrigues Advocacia</p>
                 </div>
+
+                <div className="right-header-container">
+
+                    <div className="informations-container">
+                        <span>
+                            <img src="src\assets\icons\location.svg"/>
+                            Uberaba - MG | João Aquino Silva e Oliveira, 405
+                        </span>
+
+                        <span>
+                            <img src="src\assets\icons\phone.svg"/>
+                            (34) 99161-0712
+                        </span>
+                    </div>
+
+                    <nav className="links-nav-container">
+                        <a href="#areas"><span></span>Áreas de atuação</a>
+                        <a href="#equipe"><span></span>Nossa equipe</a>
+                        <a href="#avaliacoes"><span></span>Avaliações</a>
+                        <a href="#contato"><span></span>Contato</a>
+                    </nav>
+
+                </div>
+
+            
+    
             </div>
+
+            <img src="https://lunetas.com.br/wp-content/uploads/2021/04/como-saber-se-estou-mimando-demais-o-meu-filho-portal-lunetas.jpg" alt="" />
         </header>
     );
 }
